e6cbe230f3c345167b1672bc151aafa7755e232c
vamsi <vamsirocks0007@gmail.com>
2018-08-02 18:08:05 +0530

    this is for slave branch..just messing around..:-)

--- new.cpp
+++ new.cpp
@@ -3,5 +3,6 @@ using namespace std;
 int  main(int argc, char const *argv[]) {
   std::cout << "hello vamsi..." << '\n';
 std::cout << "done this change" << '\n';
+std::cout << "this is for slave thing" << '\n';
   return 0;
 }
